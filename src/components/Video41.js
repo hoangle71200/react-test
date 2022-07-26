2cda715614a7a54e763ed7778dd9d143cbcb63d7
hoangle71200 <108779064+hoangle71200@users.noreply.github.com>
2022-07-26 13:32:55 +0700

    Create Video41.js

---
+++ src/components/Video41.js
@@ -0,0 +1,12 @@
+import {memo} from 'react'
+
+function Video41() {
+
+    return(
+        <div id="test">
+            <h2>Hello babe </h2>
+        </div>
+    )
+}
+
+export default memo(Video41)
