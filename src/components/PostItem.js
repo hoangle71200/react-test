cdb6630d35863a07c62139cb1cd44f90df6b2e3f
hoangle71200 <108779064+hoangle71200@users.noreply.github.com>
2022-07-26 12:08:35 +0700

    Create PostItem.js

---
+++ src/components/PostItem.js
@@ -0,0 +1,14 @@
+
+
+function PostItem() {
+    return(
+        <div className="post-item">
+            <img src="../image/290701058_359510502922667_1688450932766327213_n.jpg" alt="error"/>
+            <h2>Vill-V 1</h2>
+            <p>image 1</p>
+        </div>
+    )
+}
+
+
+export default PostItem
